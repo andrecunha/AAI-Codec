b996456326372d729b68659b3493f7157cb970aa
Andre Cunha <andrecunha.usp@gmail.com>
2011-09-20 15:07:20 -0300

    Modified bitio.h and bitio.c - now FIFO, not LIFO.

--- include/bitio.h
+++ include/bitio.h
@@ -1,34 +1,104 @@
 #ifndef BITIO_H
 #define BITIO_H
 
+/*! \file bitio.h
+ *  \brief Defines a FIFO bit buffer, used for general purpose bitwise storing operations.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Verificar como fica a questão de múltiplos tamanhos de blocos. */
+
+/*! \var typedef unsigned char byte
+ *  \brief A type definition to represent a byte.
+ */
 typedef unsigned char byte;
 
+/*! \struct _bitbuffer
+ *  \brief A struct that stores a FIFO bit buffer.
+ */
 struct _bitbuffer {
-	size_t size;
-	unsigned long n_bytes;
-	unsigned int bits_last;
-	byte *data;
+	size_t size; /*!< The number of bytes allocated for "data". */
+	unsigned long n_bytes; /*!< The effective number of bytes stored in the buffer (including the possibly incomplete fist and last bytes). */
+	unsigned int bits_last; /*!< The number of bits effectively used in the last byte. */
+	unsigned int bits_offset; /*!< The number of bits to de skipped in the first byte. */
+	byte *data, *original_data; /*!< The data itself. */
 };
 
 typedef struct _bitbuffer bitbuffer;
 
+/*! \fn int binit(bitbuffer *b, unsigned long size)
+ *  \brief Initializes a bit buffer.
+ *  
+ *  \param b The bit buffer.
+ *  \param size The maximum size of the buffer (the ammount of memory to be allocated).
+ *  \return 0 if the buffer could be allocated; 1 otherwise.
+ */
 int binit(bitbuffer *b, unsigned long size);
 
+/*! \fn int bwrite(bitbuffer *b, byte data)
+ *  \brief Write a bit at the end of the bit buffer.
+ *
+ *  \param b The bit buffer where the bit is to be stored.
+ *  \param data The bit to the stored.
+ *
+ *  \warning "data" should be 0 or 1.
+ *
+ *  \return 0 on success; 1 otherwise.
+ */
 int bwrite(bitbuffer *b, byte data);
 
+/*! \fn int bread(bitbuffer *b, byte *out)
+ *  \brief Reads a bit from the beginning of the bit buffer.
+ *
+ *  \param b The bit buffer that is to be read.
+ *  \param out Where the read bit will be stored.
+ *
+ *  \return 0 on success; 1 otherwise.
+ */
 int bread(bitbuffer *b, byte *out);
 
+/*! \fn int bflush(bitbuffer *b, FILE *f)
+ *  \brief Flushes the buffer to a file.
+ *
+ *  \param b The bit buffer that is to be flushed.
+ *  \param f The file where the buffer will be flushed to.
+ *
+ *  \return 0 on success; 1 otherwise.
+ */
 int bflush(bitbuffer *b, FILE *f);
 
+/*! \fn void bprint(bitbuffer *b)
+ *  \brief Prints the buffer to the standard output. Useful for debugging.
+ *
+ *  \param b The bit buffer to be printed out.
+ */
 void bprint(bitbuffer *b);
 
+/*! \fn void bdestroy(bitbuffer *b)
+ *  \brief Deallocates the allocated memory for the buffer.
+ *
+ *  \param b The bit buffer to the freed.
+ */
 void bdestroy(bitbuffer *b);
 
+/*! \fn int bfull(bitbuffer *b)
+ *  \brief Verifies whether the buffer is full.
+ *
+ *  \param b The bit buffer.
+ *  
+ *  \return 1 if "b" is full; 0 otherwise.
+ */
 int bfull(bitbuffer *b);
 
+/*! \fn int bempty(bitbuffer *b)
+ *  \brief Verifies whether the buffer is empty.
+ *
+ *  \param b The bit buffer.
+ *  
+ *  \return 1 if "b" is empty; 0 otherwise.
+ */
 int bempty(bitbuffer *b);
 
 #endif /* BITIO_H */
--- src/bitio.c
+++ src/bitio.c
@@ -3,24 +3,32 @@
 
 int binit(bitbuffer *b, size_t size)
 {
-	b->size = size;
-	b->n_bytes=0;
-	b->bits_last=0;
-	b->data = malloc(size);
-	memset(b->data, 0, size);
+	b->data = b->original_data = malloc(size);
 
-	if (b->data)
+	if (b->data) {
+		b->size = size;
+		b->n_bytes = 0;
+		b->bits_last = 0;
+		b->bits_offset = 0;
+		memset(b->data, 0, size);
 		return 0;
+	}
 	return 1;
 }
 
 int bwrite(bitbuffer *b, byte data)
 {
+	/* If buffer is full, return error. */
 	if (!b->size || bfull(b))
 		return 1;
 
-	if(b->bits_last == 8){
+	/* If the last byte is fully occupied, try to store the bit on the next byte. */
+	if (b->bits_last == 8) {
 		b->n_bytes++;
+
+		if (b->n_bytes > b->size)
+			return 1;
+
 		b->bits_last=0;
 	}
 
@@ -40,32 +48,47 @@ int bread(bitbuffer *b, byte *out)
 	if(bempty(b))
 		return 1;
 
-	*out = ((b->data[b->n_bytes-1]) >> (8-b->bits_last)) & 1;
-	
-	if(--(b->bits_last)){
-		byte aux = ~(1 << (8-b->bits_last-1));
-		b->data[b->n_bytes-1] &= aux;
-	}else{
-		b->bits_last = 8;
-		(b->n_bytes)--;
+	/* If there is only one byte, and the offset is equal to the number of bits in the last byte, than there is no bit left in the buffer. */
+	if ((b->n_bytes == 1) && (b->bits_offset+1 == b->bits_last)){
+		b->n_bytes = 0;
+		b->bits_offset = 0;
+		return 1;
 	}
-	
+
+	if (b->bits_offset == 8) {
+		b->bits_offset = 0;
+		b->data++;
+		b->n_bytes--;
+	}
+
+	*out = b->data[0];
+
+	*out >>= (8-(b->bits_offset)-1);
+
+	*out &= 1;
+
+	(b->bits_offset)++;
+
 	return 0;
 }
 
 int bflush(bitbuffer *b, FILE *f)
 {
-	fwrite(b, sizeof(b->size)+sizeof(b->n_bytes)+sizeof(b->bits_last), 1, f);
-	fwrite(b->data, sizeof(byte), b->size, f);
+	if (fwrite(b, sizeof(b->size)+sizeof(b->n_bytes)+sizeof(b->bits_last)+sizeof(b->bits_offset), 1, f) != 1)
+		return 1;
+
+	if (fwrite(b->data, sizeof(byte), b->size, f) != b->size)
+		return 1;
 
 	return 0;
 }
 
 void bprint(bitbuffer *b)
 {
-	printf("bprint: size = %d\n", b->size);
-	printf("bprint: n_bytes = %d\n", b->n_bytes);
-	printf("bprint: bits_last = %d\n", b->bits_last);
+	printf("bprint: size = %lu\n", b->size);
+	printf("bprint: n_bytes = %lu\n", b->n_bytes);
+	printf("bprint: bits_last = %u\n", b->bits_last);
+	printf("bprint: bits_offset = %u\n", b->bits_offset);
 	printf("bprint: data = ");
 
 	int i, data_h, data_l;
@@ -82,7 +105,7 @@ void bprint(bitbuffer *b)
 
 void bdestroy(bitbuffer *b)
 {
-	free(b->data);
+	free(b->original_data);
 }
 
 int bfull(bitbuffer *b)
@@ -92,6 +115,6 @@ int bfull(bitbuffer *b)
 
 int bempty(bitbuffer *b)
 {
-	return !b->n_bytes;
+	return (!b->n_bytes);
 }
 

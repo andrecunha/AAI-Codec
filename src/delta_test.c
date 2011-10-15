167a73299567e78a68e4029bf8607797b850bf31
root <root@delldreh.(none)>
2011-10-15 14:58:05 -0300

    Delta Test - Working

--- src/delta_test.c
+++ src/delta_test.c
@@ -8,66 +8,20 @@
 
 
 int main(int argc, char* argv[]){
-    uint32_t coisa[10] = {100, 120, 150, 255, 290, 390, 99, 8, 55, 76};
-    uint32_t treco[10];
+    int32_t coisa[10] = {100, 120, 150, 255, 290, 390, 99, 8, 55, 76};
+    int32_t treco[10], coisa2[10];
 
     int i;
 
     dt_encode(coisa,treco,10);
 
-    for(i=0; i<= 10; i++)
-    printf("Treco %"PRIu32"\n\t ", coisa[i]);
+    for(i=0; i< 10; i++)
+    printf("Treco %"PRId32"\n", treco[i]);
 
+    dt_decode(treco, coisa2, 10);
+    for(i=0; i<10; i++)
+    printf("Treco descomprimido %"PRId32"\n", coisa2[i]);
 
-}
+    return 0;
 
-
-// 1 2 4 3 5 7 4 3 5 7
-// 1 1 2 -1 ...
-
-
-uint32_t dt_encode(uint32_t *input, uint32_t *output, uint32_t lenght){
-
-    uint32_t delta = 0; /*The original delta is equal to zero.*/
-    uint32_t original = input[0];
-    uint32_t i;
-    uint32_t max,min;
-
-
-    max = input[0];
-    for(i = 0; i <= lenght; i++){
-    min = input[i];
-    if (max < min)
-    max = min; /* Get the bigger value among the samples in the audio file.*/
-    }
-
-    output[0]=input[0];
-
-    /*The basic delta encoding algorithm*/
-    for(i=1; i<= lenght; i++)
-        output[i] = input[i]-input[i-1];
-
-
-    /*Routine to get the largest difference between two consecutive samples in the audio file.*/
-    uint32_t max_dif = 0, dif;
-    for(i=0; i <= lenght; i++){
-        dif = input[i] - input[i-1];
-        if(dif > max_dif)
-            max_dif = dif;
-    }
-
-    return ceil(log(max_dif)/log(2));
-
-}
-
-
-/*Decode function. It will receive a delta parameter, and lenght information of the file that it will decde.*/
-void dt_decode(uint32_t *input, uint32_t *output,uint32_t lenght){
-    uint32_t i;
-    uint32_t delta = 0;
-
-    output[0] = input[0];
-
-    for(i=1; i <= lenght; i++)
-        output[i] = input[i-1]+input[i];
 }
